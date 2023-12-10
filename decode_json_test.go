7f6287754bdef196473dcb082e31bb3d05b505c9
aldisypu <aldisypu@gmail.com>
2023-12-10 15:34:26 +0700

    belajar golang json

---
+++ decode_json_test.go
@@ -0,0 +1,25 @@
+package belajar_golang_json
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestDecodeJSON(t *testing.T) {
+	jsonString := `{"FirstName":"Aldi","MiddleName":"Syah","LastName":"Putra","Age":23,"Married":false}`
+	jsonBytes := []byte(jsonString)
+
+	customer := &Customer{}
+
+	err := json.Unmarshal(jsonBytes, customer)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(customer)
+	fmt.Println(customer.FirstName)
+	fmt.Println(customer.MiddleName)
+	fmt.Println(customer.LastName)
+
+}
